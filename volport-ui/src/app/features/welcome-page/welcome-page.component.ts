c16bba54151ee729fb6b35555e84b11b85f8e7e2
Stefan Nistor <stefannistor10@gmail.com>
2023-05-26 01:06:21 +0300

    project list on project page

--- volport-ui/src/app/features/welcome-page/welcome-page.component.ts
+++ volport-ui/src/app/features/welcome-page/welcome-page.component.ts
@@ -7,8 +7,7 @@ import {Tile} from "../../core/models/tile.model";
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent {
-  welcomePageText = `Welcome to Volport\n Here you can see the courses, labs, homeworks and your grades!
-  This course has credits. Final grade formula is !`;
+  welcomePageText = `Welcome to Volport`;
   tiles: Tile[] = [
     {text: this.welcomePageText, cols: 2, rows: 4},
     {text: '', cols: 2, rows: 1},
