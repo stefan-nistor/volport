89d215c18a0fab2d1ca50efe4d46974ce2a4b9ee
Stefan Nistor <stefannistor10@gmail.com>
2023-05-25 23:35:48 +0300

    added mocked table

--- volport-ui/src/app/app.module.ts
+++ volport-ui/src/app/app.module.ts
@@ -4,8 +4,13 @@ import {SharedModule} from "./shared/shared.module";
 import {FeaturesModule} from "./features/features.module";
 import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from "./app-routing.module";
-import { HomepageComponent } from './layout/homepage/homepage.component';
-import { HeaderComponent } from './layout/header/header.component';
+import {HomepageComponent} from './layout/homepage/homepage.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {MatSortModule} from "@angular/material/sort";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
@@ -17,7 +22,8 @@ import { HeaderComponent } from './layout/header/header.component';
     SharedModule,
     FeaturesModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
